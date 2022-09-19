69313485b38ef2016c111fde7172b11dcc495b99
Aman Siid <amman.siid@gmail.com>
2022-09-19 13:57:51 -0700

    I'm done

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    srand(time(0));
+    int magic, guess, cont = 1;
+
+    while (cont == 1){
+         magic = rand() % 10 + 1;
+         cerr << magic << endl;
+         cout << "Please Guess a number" << endl;
+         for (int curTry = 0; curTry < 3; ++curTry){
+             cin >> guess;
+             if (guess == magic){
+                 cout << "You win!" << endl;
+                 break;
+             }else if (curTry < 2){
+                 cout << "Try again!" << endl;
+             }
+         }
+         if (guess != magic){
+             cout << "Looser" << endl;
+         }
+         cout << "Wanna go again? [0 for no]";
+         cin >> cont;
+     }
+    return 0;
+}
